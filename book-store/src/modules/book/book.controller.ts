209dbbafaaa7e0c4ac20c052b37bfed46a2befd4
vsantos1711 <vsantos067100@gmail.com>
2024-12-01 19:20:09 -0300

    feat: enhance BookController with CRUD operations for books

--- book-store/src/modules/book/book.controller.ts
+++ book-store/src/modules/book/book.controller.ts
@@ -1,7 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { BookService } from './book.service';
+import { CreateBookDTO } from '../dto/create-book.dto';
+import { UpdateBookDTO } from '../dto/update-book.dto';
 
-@Controller('books')
+@Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
+
+  @Post()
+  createBook(@Body() createBookDTO: CreateBookDTO) {
+    return this.bookService.createBook(createBookDTO);
+  }
+
+  @Get('list')
+  getAllBooks() {
+    return this.bookService.getAllBooks();
+  }
+
+  @Patch(':id')
+  updateBook(@Param('id') id: string, @Body() updateBookDTO: UpdateBookDTO) {
+    return this.bookService.updateBook(id, updateBookDTO);
+  }
+
+  @Delete()
+  deleteBook(@Query('id') id: string) {
+    return this.bookService.deleteAllBooks(id);
+  }
 }
