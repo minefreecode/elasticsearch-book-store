75580bf383a53560f3720e678499a6d4f4f48056
vsantos1711 <vsantos067100@gmail.com>
2024-12-02 14:12:31 -0300

    feat: integrate FileUploader component and update page styling

--- book-importer/src/app/page.tsx
+++ book-importer/src/app/page.tsx
@@ -1,8 +1,10 @@
+import FileUploader from "@/components/FileUploader";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-[#E7E7E7]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Book importer</h1>
+        <FileUploader />
       </main>
     </div>
   );
