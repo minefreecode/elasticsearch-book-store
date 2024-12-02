b3834b55239e5bffe28ffc4db206117598b783b6
vsantos1711 <vsantos067100@gmail.com>
2024-12-02 15:38:47 -0300

    feat: add CSV parsing functionality to FileUploader component and update dependencies

--- book-importer/src/components/FileUploader.tsx
+++ book-importer/src/components/FileUploader.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { useFileUpload } from "@/services/hooks";
 import { useState } from "react";
 import { useDropzone } from "react-dropzone";
+import Papa from "papaparse";
 
 export default function FileUploader() {
   const [file, setFile] = useState<File | null>(null);
+  const [jsonData, setJsonData] = useState<any[]>([]);
   const [uploadProgress, setUploadProgress] = useState<number>(0);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
@@ -13,24 +14,47 @@ export default function FileUploader() {
     maxFiles: 1,
     onDrop: (acceptedFiles: File[]) => {
       setFile(acceptedFiles[0]);
+      setJsonData([]);
       setUploadProgress(0);
     },
   });
 
-  const submitFormMutation = useFileUpload();
+  const processCsv = (file: File) => {
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      const csvText = e.target.result;
+      try {
+        Papa.parse(csvText, {
+          complete: (result) => {
+            setJsonData(result.data);
+          },
+          header: true,
+          skipEmptyLines: true,
+        });
+      } catch (error) {
+        console.error("Erro ao processar o CSV:", error);
+      }
+    };
 
-  const handleUpload = async () => {
-    if (!file) return;
+    reader.onprogress = (e: ProgressEvent<FileReader>) => {
+      if (e.lengthComputable) {
+        const progress = Math.round((e.loaded / e.total) * 100);
+        setUploadProgress(progress);
+      }
+    };
 
-    try {
-      submitFormMutation.mutate(file);
-    } catch (error) {
-      console.error("Erro ao enviar o arquivo:", error);
+    reader.readAsText(file);
+    console.log(jsonData);
+  };
+
+  const handleUpload = () => {
+    if (file) {
+      processCsv(file);
     }
   };
 
   return (
-    <div className="min-w-[420px] mx-auto p-6 bg-white rounded-lg shadow-md">
+    <div className="w-[420px] mx-auto p-6 bg-white rounded-lg shadow-md">
       <h2 className="text-xl font-semibold mb-4 text-black">
         Import books from a CSV file
       </h2>
@@ -47,14 +71,14 @@ export default function FileUploader() {
         ) : (
           <p className="text-gray-600">
             Drag & Drop or{" "}
-            <span className="text-blue-500  hover:underline">Choose File</span>{" "}
+            <span className="text-blue-500 hover:underline">Choose File</span>{" "}
             to upload (.csv)
           </p>
         )}
       </div>
 
       {uploadProgress > 0 && (
-        <div className="relative pt-1">
+        <div className="relative pt-1 mb-4">
           <div className="flex mb-2 items-center justify-between">
             <div>
               <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded text-blue-600 bg-blue-200">
@@ -67,7 +91,7 @@ export default function FileUploader() {
               </span>
             </div>
           </div>
-          <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-200">
+          <div className="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
             <div
               style={{ width: `${uploadProgress}%` }}
               className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-600"
