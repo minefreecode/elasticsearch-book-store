e40b554eaca20efe37bab6fd2f5c9194475c726c
vsantos1711 <vsantos067100@gmail.com>
2024-12-03 11:52:51 -0300

    feat: add JSON body parser with a 50mb limit to the main application

--- book-store/src/main.ts
+++ book-store/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.use(json({ limit: '50mb' }));
+
   await app.listen(process.env.PORT ?? 3000, () => {
     console.log(
       `📘 Book-store app is running on http://localhost:${process.env.PORT ?? 3000}`,
